1cebad3be1550cb4cb1913825fb05ae9e75a566a
Yevhenii Pozniak <jekamanu@gmail.com>
2024-03-19 17:08:44 +0200

    error

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,16 @@ import "./index.css";
 import { theme } from "./components/helpers/Theme.js";
 import { ThemeProvider } from "styled-components";
 import { Provider } from "react-redux";
-import { store } from "./redux/store.js";
+import { persistor, store } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </ThemeProvider>
   </React.StrictMode>
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,27 @@ import { configureStore } from "@reduxjs/toolkit";
 import { contactsReducer } from "./contactsSlice";
 import { filterReducer } from "./filterSlice";
 
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+// це конфігурація для localStorage
+const contactsPersistConfig = {
+  key: "contacts",
+  storage,
+  // whitelist: ["contacts"],
+};
+
+const persistedContactsReducer = persistReducer(
+  contactsPersistConfig,
+  contactsReducer
+);
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
+    contacts: persistedContactsReducer,
     filter: filterReducer,
   },
 });
+
+export const persistor = persistStore(store);
