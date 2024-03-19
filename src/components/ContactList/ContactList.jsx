9806926fc0c2217b01acf584eeda8f5e9b8802cd
Yevhenii Pozniak <jekamanu@gmail.com>
2024-03-19 07:16:27 +0200

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -3,6 +3,16 @@ import Contact from "../Contact/Contact";
 
 export default function ContactList() {
   const contacts = useSelector((state) => state.contacts);
+  const filter = useSelector((state) => state.filter);
+  console.log(filter);
+
+  // const filteredContacts = () => {
+  //   const normalizedFilter = filter.toLowerCase();
+
+  //   return contacts.filter((contact) =>
+  //     contact.name.toLowerCase().includes(normalizedFilter)
+  //   );
+  // };
 
   return (
     <ul>
